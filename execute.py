0c11e59aef3295d2e8e74b8459101163d2fcc101
Abhinav Kalyan Talluri <113676352+abhinavjff@users.noreply.github.com>
2025-11-30 19:43:40 +0530

    Coordinator for the entire application.
    Corrected comment punctuation in execute.py

--- execute.py
+++ execute.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # 1. Add the 'backend' folder to Python's path
-# This allows us to import modules that live inside the backend/ directory
+# This allows us to import modules that live inside the backend/ directory .
 sys.path.append(os.path.join(os.path.dirname(__file__), "backend"))
 
 # 2. Import the main function from your CLI tool
